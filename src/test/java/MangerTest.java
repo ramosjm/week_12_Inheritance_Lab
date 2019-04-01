b3e22c85bc86f3b497e0958b051f1621e0ecc0a9
ramosjm <jjramos_2004@hotmail.com>
2019-04-01 23:22:28 +0100

    can raise salary test passed.

--- src/test/java/MangerTest.java
+++ src/test/java/MangerTest.java
@@ -30,7 +30,13 @@ public class MangerTest {
 
     @Test
     public void canPayBonus(){
-        assertEquals(100.4*0.01,manager.payBonus(), 0.001);
+        assertEquals(100.40*0.01,manager.payBonus(), 0.001);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(10.00);
+        assertEquals(110.40,manager.getSalary(),0.001);
     }
 
 }
