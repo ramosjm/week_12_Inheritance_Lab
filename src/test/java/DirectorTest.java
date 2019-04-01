3b9bb4752387fca7a2cea3ced5a1b0f254317f54
ramosjm <jjramos_2004@hotmail.com>
2019-04-02 00:13:20 +0100

    director tests passed.

--- src/test/java/DirectorTest.java
+++ src/test/java/DirectorTest.java
@@ -14,6 +14,31 @@ public class DirectorTest {
     }
 
     @Test
+    public void canGetNiNumber(){
+        assertEquals("AA111",director.getNiNumber());
+    }
+    @Test
+    public void canGetSalary(){
+        assertEquals(300.20,director.getSalary(), 0.001);
+    }
+
+    @Test
+    public void canGetDeptName(){
+        assertEquals("Sales",director.getDeptName());
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(300.20*0.01,director.payBonus(), 0.001);
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        director.raiseSalary(10.00);
+        assertEquals(310.20,director.getSalary(),0.001);
+    }
+
+    @Test
     public void canGetBudget(){
         assertEquals(5000.00,director.getBudget(),0.001);
     }
