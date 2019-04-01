450a523bd866169381911a7e4099e1aa83312680
ramosjm <jjramos_2004@hotmail.com>
2019-04-01 23:53:54 +0100

    developer can getni number test passed.

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("cb345",250.20);
+    }
+
+    @Test
+    public void canGetNiNumber(){
+        assertEquals("cb345",developer.getNiNumber());
+    }
+
+
 }
